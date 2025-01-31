33de098fb80533f315042d72a323d96e0374a652
IDGS-803-19002425 <19002425@alumnos.utleon.edu.mx>
2025-01-30 22:17:37 -0600

    2 formularios

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request
+ 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola mundo nuevo Flask"
+    grupo = "IDGS803"
+    lista = ["Aaron","Hernan","Rocha"]
+    return render_template('index.html',grupo=grupo,lista=lista)
+
+@app.route("/operasBas")
+def operas():
+        return render_template('OperasBas.html')
+    
+
+
+@app.route("/operasBas",methods=['POST',"GET"])
+def resultado():
+      resultado = 0
+      if request.method == 'POST':
+        opcion = request.form.get('opcion')
+        numero1 = int(request.form.get('numero1'))
+        numero2 = int(request.form.get('numero2'))
+        if opcion == "suma":
+            resultado = numero1 + numero2
+        if opcion == "resta":
+            resultado = numero1 - numero2
+        if opcion == "multiplicacion":
+            resultado = numero1 * numero2
+        if opcion == "division":
+            resultado = numero1 / numero2        
+        return render_template('OperasBas.html',resultado=resultado)
+      
+@app.route("/ejemplo1")
+def ejemplo1():
+    return render_template('ejemplo1.html')
+
+@app.route("/ejemplo2")
+def ejemplo2():
+    return render_template('ejemplo2.html')
+
 
 @app.route("/Hola")
 def hola():
@@ -31,7 +66,20 @@ def suma(n1,n2):
 def func(nom="Aaron"):             
        return "El nombre es: {}".format(nom)
 
+@app.route("/foem1")
+def form1():
+       return  '''
+        <form>
+        <label>Nombre:</label>
+        <input type="text" name="nombre" placeholder="Nombre">
+        </br>
+        <label>Nombre:</label>
+        <input type="text" name="nombre" placeholder="Nombre">
+        </br>
+        </form>
+        '''
+
+
 if __name__ == '__main__':
     app.run(debug=True,port=3000)
-
 
